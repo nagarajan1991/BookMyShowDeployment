32ba6ea75cb146b3998fc3448bf562d41d539195
nagarajan1991 <nagarajan1991@gmail.com>
2025-04-21 23:27:06 +0100

    Corrected typo in server.js

--- server/server.js
+++ server/server.js
@@ -1,64 +1,66 @@
 const express = require('express');
-import * as cors from 'cors';
+const cors = require("cors");
+const path = require("path");
 
 // Load env Variables.
 require('dotenv').config();
 
 // Constants
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 // Setup
 const app = express();
 
-const clientBuildPath = Path2D.join(__dirname, '../client/build');
+// Serve static files from the React build
+const clientBuildPath = path.join(__dirname, "../client/build");
 app.use(express.static(clientBuildPath));
-app.get('*', (req, res) => {
-    res.sendFile(Path2D.join(clientBuildPath, 'index.html'));
-});
 
+
+// CORS
 app.use(
     cors({
-        origin: ["http://localhost:3000","https://bookmyshowdeployment.onrender.com"],
+        origin: ["http://localhost:3000", "https://bookmyshowdeployment.onrender.com"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
     })
-)
+);
 
+// Middleware
 app.use(express.json()); 
 
-// Routes
-app.use("/api/users", userRoute);
-
-// Data base connection.
+// DB Connection
 const connectDb = require("./config/db");
-connectDb(); // Stablish database connection.
+connectDb(); // Establish database connection
 
-// Global Variables
+// Routes
 const USER_ROUTER = require("./routes/userRoute");
 const MOVIE_ROUTER = require("./routes/movieRoute");
 const THEATER_ROUTER = require("./routes/theatreRoute");
 const SHOW_ROUTER = require("./routes/showRoute");
 const BOOKING_ROUTER = require("./routes/bookingRoute");
 
-// Routes.
 app.use("/api/users", USER_ROUTER);
 app.use("/api/movies", MOVIE_ROUTER);
 app.use("/api/theatres", THEATER_ROUTER);
 app.use("/api/shows", SHOW_ROUTER);
 app.use("/api/bookings", BOOKING_ROUTER);
 
+// Default Route
 app.get("/", (req, res) =>
-    res.status(201).send("Welcome to the home page.")
+    res.status(200).send("Welcome to the home page.")
 );
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(clientBuildPath, 'index.html'));
+});
+
+// 404 Handler
 app.use((req, res) =>
     res.status(404).json({ message: "page not found" })
 );
 
-// Start the server.
-// URL: localhost:8082
+// Start the server
 app.listen(PORT, () => {
-    console.log(`server is running on port`);
+    console.log(`Server is running on port ${PORT}`);
 });
-
